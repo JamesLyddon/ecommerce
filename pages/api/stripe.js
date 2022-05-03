844445b5670cd1dc4dfd7193db28c41d61ea6f02
JamesLyddon <jameslyddon@gmail.com>
2022-05-03 14:37:52 +0100

    before undo pull fix

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,13 +1,11 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // import Stripe from "stripe";
-
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     console.log(req.body.cartItems);
-
     try {
       const params = {
         submit_type: "pay",
