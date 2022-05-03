5977f742696f0d2b1bd3b28188404c0438510def
JamesLyddon <jameslyddon@gmail.com>
2022-05-03 15:07:11 +0100

    still stuck

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -38,7 +38,7 @@ const Cart = () => {
     if (response.statusCode === 500) return;
 
     const data = await response.json();
-
+    
     toast.loading("Redirecting...");
 
     stripe.redirectToCheckout({ sessionId: data.id });
