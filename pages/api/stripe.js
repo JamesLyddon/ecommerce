ab2e918ef361ebb1835690f172b78613dd01af41
JamesLyddon <jameslyddon@gmail.com>
2022-05-05 13:08:13 +0100

    pipe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -26,8 +26,8 @@ export default async function handler(req, res) {
       };
 
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      // const session = await stripe.checkout.sessions.create(params);
+      // res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
